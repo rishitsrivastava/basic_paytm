6e4077d3e2e83e866d440d97735a341f6d0add27
rishitsrivastava <rishitsrivastava43@gmail.com>
2024-01-24 00:21:18 +0530

    added account schema and model in db.js

--- backend/db.js
+++ backend/db.js
@@ -4,14 +4,50 @@ const { URL } = require("./url");
 mongoose.connect(URL);
 
 const userSchema = mongoose.Schema({
-    username: String,
-    password: String,
-    firstName: String,
-    lastName: String,
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true,
+        minLength: 3,
+        maxLength: 30
+    },
+    password: {
+        type: String,
+        required: true,
+        minLength: 6
+    },
+    firstName: {
+        type: String,
+        required: true,
+        trim: true,
+        maxLength: 50
+    },
+    lastName: {
+        type: String,
+        required: true,
+        trim: true,
+        maxLength: 50
+    }
 })
 
+const accountSchema = new mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    balance: {
+        type: number,
+        required: true
+    }
+});
+
 const User = mongoose.model("User", userSchema);
+const Account = mongoose.model('Account', accountSchema)
 
 module.exports = {
-    User
+    User,
+    Account
 }
