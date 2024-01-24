110a128eed22d5d288c54787c299b9619d57d0bf
rishitsrivastava <rishitsrivastava43@gmail.com>
2024-01-24 15:43:38 +0530

    added a route to implement transaction

--- backend/routes/account.js
+++ backend/routes/account.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const { Account } = require("../db");
+const { authMiddleware } = require("../middleware");
 const router = express.Router();
 
 
-router.get("/balance", async(req, res) => {
+router.get("/balance", async (req, res) => {
     const account = await Account.findOne({
         userId: req.body.userId
     });
@@ -11,6 +12,44 @@ router.get("/balance", async(req, res) => {
         balance: account.balance
     })
 })
+
+router.post("/transfer", authMiddleware , async (req, res) => {
+    const { amount, to } = req.body;
+
+    const account = await Account.findOne({
+        userId: req.userId
+    });
+    if(account.balance < amount) {
+        return res.status(400).json({
+            message: "insufficient balance"
+        })
+    }
+    const toAccount = await Account.findOne({
+        userId: to
+    })
+    if(!toAccount) {
+        return res.status(400).json({
+            message: "Invalid Account"
+        })
+    }
+    await Account.updateOne({
+        userId: req.userId
+    }, {
+        $inc: {
+            balance: -amount
+        }
+    })
+    await Account.updateOne({
+        userId: to
+    }, {
+        $inc: {
+            balance: amount
+        }
+    })
+    res.json({
+        message: "Transfer successful"
+    })
+})  
 
 
 
