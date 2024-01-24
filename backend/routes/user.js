e98a37e8d99f801f1ebf737bc0f3f120a60e213d
rishitsrivastava <rishitsrivastava43@gmail.com>
2024-01-24 22:08:49 +0530

    correction

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -9,8 +9,8 @@ const { authMiddleware } = require("../middleware")
 const signupbody = zod.object({
     username: zod.string().email(),
     password: zod.string(),
-    firstname: zod.string(),
-    lastname: zod.string()
+    firstName: zod.string(),
+    lastName: zod.string()
 })
 const signinbody = zod.object({
     username: zod.string().email(),
@@ -30,30 +30,25 @@ userRouter.post("/signup", async (req, res) => {
             message: "Email already taken / Incorrect inputs"
         })
     }
-
     const existingUser = await User.findOne({
         username: req.body.username
     })
-
     if (existingUser) {
         return res.status(411).json({
             message: "Email already taken/Incorrect inputs"
         })
     }
-
     const user = await User.create({
         username: req.body.username,
         password: req.body.password,
-        firstname: req.body.firstname,
-        lastname: req.body.lastname,
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
     })
     const userId = user._id;
-
     await Account.create({
         userId,
         balance: 1 + Math.random() * 10000
     })
-
     const token = jwt.sign({
         userId
     }, JWT_Secret);
