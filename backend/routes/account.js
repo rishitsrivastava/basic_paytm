732b822facdb103ba446999a2e9e4742913466cf
rishitsrivastava <rishitsrivastava43@gmail.com>
2024-01-27 22:50:32 +0530

    fixed the bugs in backend

--- backend/routes/account.js
+++ backend/routes/account.js
@@ -6,7 +6,7 @@ const router = express.Router();
 
 router.get("/balance", authMiddleware , async (req, res) => {
     const account = await Account.findOne({
-        userId: req.body.userId
+        userId: req.userId
     });
     res.json({
         balance: account.balance
