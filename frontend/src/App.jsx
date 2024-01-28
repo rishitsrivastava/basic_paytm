1e3bc359e56694979cba121f5fc415a22b178bec
rishitsrivastava <rishitsrivastava43@gmail.com>
2024-01-28 22:34:11 +0530

    created send.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,21 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Signup from './components/Signup'
-// import Signin from './components/Signin';
-// import Dashboard from './components/Dashboard';
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Signup from './pages/Signup'
+import Signin from './pages/Signin'
+import Dashboard from './pages/Dashboard'
+import Send from './pages/Send'
 
-function App() {
-
+export default function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/signup" element={<Signup />} /> 
-        {/* <Route path="/signin" element={Signin} />
-        <Route path="/dashboard" element={Dashboard} /> */}
-      </Routes>
-    </Router>
-  //   <div>
-  //     <Signup />
-  //   </div>
-   )
+    <>
+        <BrowserRouter>
+          <Routes>
+            <Route path='/signup' element={<Signup />} />
+            <Route path='/signin' element={<Signin />} />
+            <Route path='/dashboard' element={<Dashboard />} />
+            <Route path='/send' element={<Send />} />
+          </Routes>
+        </BrowserRouter>
+    </>
+  )
 }
-
-export default App
