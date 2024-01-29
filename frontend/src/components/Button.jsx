d3fe13df374f6c5e81b6fa2a4c01528a2ffa5bb7
rishitsrivastava <rishitsrivastava43@gmail.com>
2024-01-29 15:43:44 +0530

    completed signup page

--- frontend/src/components/Button.jsx
+++ frontend/src/components/Button.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Button({label, onClick}) {
   return (
-    <button onClick={onClick} type="button" class=" w-full mt-4 text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2">
+    <button onClick={onClick} type="button" class=" w-full text-white bg-gray-950 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2">
       {label}
     </button>
   )  
--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,24 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Heading from '../components/Heading'
 import SubHeading from '../components/SubHeading'
 import InputBox from '../components/InputBox'
 import Button from '../components/Button'
 import ButtonWarning from '../components/ButtonWarning'
+import { useNavigate } from 'react-router-dom'
 
 export default function Signup() {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [userName, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
   return (
-    <div className='bg-slate-300 flex justify-center h-screen'>
-      <div className='flex flex-col justify-center'>
-        <Heading label={"Sign up"} />
-        <SubHeading label={"Enter your information to create an account"} />
-        <InputBox placeholder="John" label={"First Name"} />
-        <InputBox placeholder="Doe" label={"Last Name"} />
-        <InputBox placeholder="rishit111@gmail.com" label={"First Name"} />
-        <InputBox placeholder="123456" label={"Password"} />
-        <div>
-          <Button label={"Sign up"} />
+    <div className='bg-slate-900 flex justify-center h-screen'>
+      <div className='flex flex-col shadow-2xl justify-center'>
+        <div className='rounded-lg bg-slate-800 text-white w-80 text-center p-2 h-max px-4'>
+          <Heading label={"Sign up"} />
+          <SubHeading label={"Enter your information to create an account"} />
+          <InputBox onChange={(e) => {setFirstName(e.target.value)}} placeholder="John" label={"First Name"} />
+          <InputBox onChange={(e) => {setLastName(e.target.name)}} placeholder="Doe" label={"Last Name"} />
+          <InputBox onChange={(e) => {setUserName(e.target.value)}} placeholder="rishit111@gmail.com" label={"First Name"} />
+          <InputBox onChange={(e) => {setPassword(e.target.value)}} placeholder="123456" label={"Password"} />
+        <div className='pt-4'>
+          <Button 
+            onClick={async () => {
+              const response = await axios.post("http://localhost:5173/api/vi/user/signup", {
+                userName,
+                firstName,
+                lastName,
+                password
+              });
+              localStorage.setItem("token", response.data.token)
+              navigate("/dashboard")
+            }}
+            label={"Sign up"} />
         </div>
         <ButtonWarning label={"Already have an account"} buttonText={"Sign in"} to={"/signin"} />
+       </div>
       </div>
     </div>
   )
