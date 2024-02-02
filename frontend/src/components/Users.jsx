445bc02efe5d8c9228595419ed9d8b5a0eb9cc7f
rishitsrivastava <rishitsrivastava43@gmail.com>
2024-02-02 22:43:40 +0530

    completed send component

--- frontend/src/components/Users.jsx
+++ frontend/src/components/Users.jsx
@@ -46,7 +46,7 @@ function User({user}) {
     return <div className="flex justify-between mb-4">
         <div className="flex">
             <div className="rounded-full h-12 w-12 bg-slate-200 flex justify-center mt-1 mr-2">
-                <div className="flex flex-col justify-center text-slate-600 h-full text-xl">
+                <div className="flex flex-col justify-center text-slate-600 h-full text-xl font-semibold">
                     {user.firstName[0]}
                 </div>
             </div>
--- frontend/src/pages/Send.jsx
+++ frontend/src/pages/Send.jsx
@@ -1,7 +1,38 @@
 import React from 'react'
+import Button from '../components/Button'
 
 export default function Send() {
+
   return (
-    <div>Send</div>
+    <div className='bg-slate-950 h-screen flex justify-center items-center'>
+      <div className='flex justify-center flex-col shadow-2xl text-slate-300 bg-slate-900 p-8'>
+        <div className='text-lg flex items-center justify-center font-semibold'>
+          Send Money
+        </div>
+        <div className='mt-4'>
+          <div className='flex mt-5'>
+            <div className='bg-slate-400 text-slate-800 rounded-full w-8 h-8'>
+              <div className='ml-2.5 mt-0.5 text-lg font-semibold'>
+                R
+              </div>
+            </div>
+            <div className='ml-2 mt-1'>
+                Friend's Name
+            </div>
+          </div>
+        </div>
+        <div className='mt-3'>
+          <div>
+            Amount (in Rs)
+          </div>
+          <div className='mt-2'>
+            <input placeholder='Enter amount' className='rounded-lg pl-2' />
+          </div>
+        </div>
+        <div className='mt-6'>
+          <Button label={"Send"} />
+        </div>
+      </div>
+    </div>
   )
 }
